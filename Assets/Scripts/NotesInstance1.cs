3f0684c9bae995ccf058bcb82f042c95ce290da6
takosuu <87427744+takosuu@users.noreply.github.com>
2021-08-04 22:58:49 +0900

    音に合わせて反応させる（ノーツ生成の下地）

--- Assets/Scripts/NotesInstance1.cs
+++ Assets/Scripts/NotesInstance1.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NotesInstance1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+      public void   NoteEvent()
+        {
+        Debug.Log("");
+        }
     }
-}
