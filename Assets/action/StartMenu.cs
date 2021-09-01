8312fcf37b3d1f0b96ff9a2c364fee0350df63e6
DESKTOP-AAVLGPG\beru2 <beru2@outlook.jp>
2021-09-01 22:55:44 +0900

    no message

---
+++ Assets/action/StartMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void Onclick()
+    {
+        //ここで移りたいシーンを指定します。
+        SceneManager.LoadScene("Stage");
+    }
+}
